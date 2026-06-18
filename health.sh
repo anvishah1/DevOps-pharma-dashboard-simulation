79aedc4edc6195dc399ab1d64e4b97166bb44d8f
anvishah1 <anvi78424@gmail.com>
2026-06-18 16:27:20 +0000

    health summary script added

---
+++ health.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo "---SERVER HEALTH REPORT ---"
+
+disk=$(df -h / | awk 'NR==2 {print $5}' | tr -d  '%')
+if [ $disk -lt 80 ]; then
+	echo "Disk: OK($disk%)"
+elif [ $disk -ge 80 ] && [ $disk -le 90 ]; then
+	echo "Disk: WARNING ($disk%)"
+else
+	echo "Disk: CRITICAL ($disk%)"
+fi
+
+mem=$(free | awk 'NR==2 {printf "%.0f", $3/$2 * 100}')
+if [ $mem -lt 75 ]; then
+	echo "Memory: OK ($mem%)"
+elif [ $mem -ge 75 ] && [ $mem -le 90 ]; then
+	echo "Memory: WARNING ($mem%)"
+else
+	echo "Memory: CRITICAL ($mem%)"
+fi
+
+cpu=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+cpu=${cpu%.*}
+if [ $cpu -lt 70 ]; then
+	echo "CPU: OK ($cpu%)"
+elif [ $cpu -ge 70 ] && [ $cpu -le 85 ]; then
+	echo "CPU: WARNING ($cpu%)"
+else
+	echo "CPU: CRITICAL ($cpu%)"
+fi
+
+service=$(systemctl is-active nginx)
+if [ "$service" = "active" ]; then
+	echo "Nginx: OK(active)"
+else
+	echo "Nginx: CRITICAL(inactive)"
+fi
+
+ping -c 1 google.com > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+	echo "Network: OK"
+else
+	echo "Network: CRITIAL"
+fi
+
+
